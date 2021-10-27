56143416426c1e2c73f180c53bb5b03bdfda84e1
williams-jacobr <williams.jacobr@gmail.com>
2021-10-27 17:06:14 +0300

    practicing core angular features and working with subscriptions

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-create',
@@ -6,10 +9,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./post-create.component.css'],
 })
 export class PostCreateComponent {
-  enteredValue = '';
-  newPost = 'NO CONTENT';
+  enteredContent = '';
+  enteredTitle = '';
 
-  onAddPost() {
-    this.newPost = this.enteredValue;
+  constructor(public postsService: PostsService) {}
+
+  onAddPost(form: NgForm) {
+    if (form.invalid) return;
+
+    this.postsService.addPosts(form.value.title, form.value.content);
+    form.resetForm();
   }
 }
